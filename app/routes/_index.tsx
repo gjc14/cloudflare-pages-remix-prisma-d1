c7345d72dafe9c04657001c61e2f6244f8625775
gjc14 <yin@yinc.me>
2024-09-23 20:26:26 -0500

    style: / and users page chore: delete generated data; update README

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,7 +3,7 @@ import {
   type LoaderFunctionArgs,
   type MetaFunction,
 } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -34,8 +34,26 @@ export default function Index() {
   console.log(results);
 
   return (
-    <main>
-      <h1>Remix + Prisma + Cloudflare D1</h1>
+    <main className="m-8 space-y-5">
+      <h1 className="text-2xl font-bold">Remix + Prisma + Cloudflare D1</h1>
+
+      <p>
+        Result of{" "}
+        <code className="text-sm bg-amber-300 px-1">
+          await env.DB.prepare('SELECT * FROM user LIMIT 5').all()
+        </code>{" "}
+        in loader
+      </p>
+
+      <p className="px-3 py-2 bg-gray-200 rounded-[8px]">
+        {JSON.stringify(results)}
+      </p>
+
+      <div>
+        <Link to="users" className="font-semibold hover:underline">
+          {">"} Checkout Users Query with Prisma
+        </Link>
+      </div>
     </main>
   );
 }
--- app/routes/users.tsx
+++ app/routes/users.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderFunctionArgs } from "@remix-run/cloudflare";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { getUsers } from "~/db/user.server";
 
 export const loader = async ({ context }: LoaderFunctionArgs) => {
@@ -12,9 +12,26 @@ export const loader = async ({ context }: LoaderFunctionArgs) => {
 export default function User() {
   const { users } = useLoaderData<typeof loader>();
   return (
-    <main>
-      <h1>Users</h1>
-      <p>Result: {JSON.stringify(users)}</p>
+    <main className="m-8 space-y-5">
+      <h1 className="text-2xl font-bold">Users</h1>
+
+      <p>
+        Result of{" "}
+        <code className="text-sm bg-amber-300 px-1">
+          await prisma.user.findMany();
+        </code>{" "}
+        in loader:{" "}
+      </p>
+
+      <p className="px-3 py-2 bg-gray-200 rounded-[8px]">
+        {JSON.stringify(users)}
+      </p>
+
+      <div>
+        <Link to="/" className="font-semibold hover:underline">
+          {">"} Back home
+        </Link>
+      </div>
     </main>
   );
 }
